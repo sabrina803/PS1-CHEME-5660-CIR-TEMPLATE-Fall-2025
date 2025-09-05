75f5300f5d05380b1831abcaf489424e5cc98d02
Jeffrey D Varner <jdv27@cornell.edu>
2025-09-05 14:05:07 -0400

    Update Types.jl
    initial type

---
+++ src/Types.jl
@@ -0,0 +1,21 @@
+"""
+    mutable struct  MyGeneralRecombiningTreeModel <: AbstractPriceTreeModel
+
+The `MyGeneralRecombiningTreeModel` type is a model of a commodity price tree that uses an dictionary to store the price data.
+This model stores the connectivity information between nodes.
+
+### Fields
+- `data::Union{Nothing, Dict{Int64,NamedTuple}}` - A dictionary that stores the price data and path informationfor the tree.
+- `connectivity::Dict{Int64,Array{Int64,1}}` - A dictionary that stores the connectivity information between nodes.
+"""
+mutable struct MyGeneralRecombiningTreeModel <: AbstractPriceTreeModel
+
+    # data -
+    data::Union{Nothing, Dict{Int64,NamedTuple}}
+    connectivity::Dict{Int64,Array{Int64,1}}
+    h::Int64 # height of the tree
+    n::Int64 # branching factor
+
+    # constructor 
+    MyGeneralRecombiningTreeModel() = new()
+end
