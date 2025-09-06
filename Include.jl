8d0d2ae598d492c0dc15a5756e01b7291ae7e2b5
Jeffrey D Varner <jdv27@cornell.edu>
2025-09-06 19:15:42 -0400

    Refactor and reorganize project structure
    Renamed Include-solution.jl to Include.jl and updated path setup and file includes. Removed solution and src module files (Factory.jl, Trees.jl, Types.jl) and replaced with Files.jl and Utility.jl. Added new notebook files for student and solution, updated LICENSE year, modified Project.toml and README.md, and added new Treasury dataset CSV files and solution data. Cleaned up legacy solution files and improved environment setup.

--- Include.jl
+++ Include.jl
@@ -1,12 +1,12 @@
-# setup my paths -
-const _PATH_TO_ROOT = @__DIR__;
-const _PATH_TO_SRC = joinpath(_PATH_TO_ROOT, "src");
-const _PATH_TO_DATA = joinpath(_PATH_TO_ROOT, "data");
-
+# setup paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_SOLN = joinpath(_ROOT, "soln");
 
 # make sure all is up to date -
 using Pkg
-if (isfile(joinpath(_PATH_TO_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
     Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
     Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 end
@@ -25,10 +25,10 @@ using StatsPlots
 using JLD2
 using FileIO
 using Distributions
+using DataStructures
 using PrettyTables
 using Test
 
-# load my files -
-include(joinpath(_PATH_TO_SRC, "Types.jl"));
-include(joinpath(_PATH_TO_SRC, "Factory.jl"));
-include(joinpath(_PATH_TO_SRC, "Trees.jl"));
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Files.jl"));
+include(joinpath(_PATH_TO_SRC, "Utility.jl"));
